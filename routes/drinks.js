1c3a645a22689b4768d8e6da8e38fad35e2aafbf
dubemelchi <dubemelchi@gmail.com>
2019-09-05 16:35:22 -0400

    added drinks to table in db

--- routes/drinks.js
+++ routes/drinks.js
@@ -12,3 +12,22 @@ Drink.findAll()
   .catch(err => console.log(err)));
 
 module.exports = router;
+
+// add drink
+router.get('/add', (req, res) => {
+  const data = {
+    drink_name: 'Modelo',
+    drink_cost: 8.00
+  }
+
+
+let { drink_name, drink_cost } = data;
+
+// insert into table
+Drink.create({
+  drink_name,
+  drink_cost
+})
+.then(drink => res.redirect('/drinks'))  
+.catch(err => console.log(err));
+});
